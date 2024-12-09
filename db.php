ce08b0372cf146afdfcbec88b3a694f47c33c06a
04enkat <122863624+04enkat@users.noreply.github.com>
2024-12-09 11:17:26 +0530

    update index php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $dbname = 'siva_game';
 $username = 'root';
 $password = '';
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli(hostname: $host, username: $username, password: $password, database: $dbname);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
